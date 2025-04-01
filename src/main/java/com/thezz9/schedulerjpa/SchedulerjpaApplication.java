1b8d56959bb69dabb75bdc20d72f5c0ddd8a24a6
thezz9 <thezz9@naver.com>
2025-04-01 13:16:23 +0900

    Create: 프로젝트 생성 date: 2025/04/01

---
+++ src/main/java/com/thezz9/schedulerjpa/SchedulerjpaApplication.java
@@ -0,0 +1,13 @@
+package com.thezz9.schedulerjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerjpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerjpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thezz9/schedulerjpa/SchedulerjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thezz9.schedulerjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerjpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
